dd0ae52e24d937b7319293b3569d248d928ebc88
smil1201 <mill5978@morris.umn.edu>
2017-12-05 05:11:09 -0600

    Added more structure to main function, created stub function for storing data.

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
 
@@ -46,7 +47,9 @@ public class Main {
 			for (int i = 0; i < 20; i++) {
 				//while(!f1Done && !f2Done && !f3Done) {
 				byte[] inByteData = new byte[1028];
-
+				String filename = "";
+				boolean lastData = false;
+				
 				//set inbyte Data
 				DatagramPacket inPacket = new DatagramPacket(inByteData, inByteData.length);
 
@@ -56,46 +59,60 @@ public class Main {
 
 				//------------------------------------
 				//test string receiving functionality
-				String testText = new String(inByteData, 0, inPacket.getLength());
+				//String testText = new String(inByteData, 0, inPacket.getLength());
 
 				//System.out.println("byte: " + Integer.toBinaryString(inByteData[0]));
+				//System.out.println("byte: " + inByteData[0]);
 				//System.out.println("packet text: " + testText);
-
+				//-------------------------------------
+				
 				//add file id if it is new
-				if (IDArr.contains((int) inByteData[1])!=true) {
-					IDArr.add((int) inByteData[1]);
+				int fileID = ((int) inByteData[1]);
+				if (IDArr.contains(fileID)!=true) {
+					IDArr.add(fileID);
 				}
+				
 				checkFileCompletion(inPacket, IDArr);
-
 
 				//check if header (the last bit of the status byte)
 				if(inByteData[0]==0){					
 					byte[] fn = Arrays.copyOfRange(inByteData, 2, inPacket.getLength());
-					String s = new String(fn, "US-ASCII");
-					filenameArr.add(s);
-				}
+					filename = new String(fn, "US-ASCII");
+					filenameArr.add(filename);
 				
-				//} else {
-				//it is a data file
-
-				//packet number is bytes 3-4, data is every byte after 4 
-
-				//check if it is the last data packet
-				//if (the 2nd to last bit of inByteData[0] is 1) {
-
-				//set last packet field to true
-
-				//} 
-				//store data in respective file Arraylist
-				//}
+				} else {
+					//it is a data file
+					//packet number is bytes 3-4, data is every byte after 4 
+					//check if it is the last data packet
+					if(inByteData[0]==3){
+						lastData = true;
+					} 
+					//store packet data
+					byte[] data = Arrays.copyOfRange(inByteData, 4, inPacket.getLength());
+					
+					//TODO: write "storeData" function to check file id and add to respective array list
+					//storeData(inpacket, fileID)
+					
+					//for now add to file1 arrayList for testing	
+					f1Bytes.add(data);
+				}
 
 			}    
 			//all three ArrayLists should contain respective files data
 
-			//sort f1Bytes, f2Btyes, f3Btyes by packet number
+			//sort f1Bytes, f2Btyes, f3Bytes by packet number
+			//Collections.sort(f1Bytes, new Comparator<packet>() {
+			//    @Override
+			//    public int compare(packet left, packet right) {
+			//        return left.packNum > right.packNum ? 1 : (left.packNum < right.packNum) ? -1 : 0;
+			//    }
+			//}
 			
 			//write files out to their filenames
-
+			//try (FileOutputStream os = new FileOutputStream("relative path" + filenameArr[0])) {
+			//	   os.write(f1Bytes);
+			//	   os.close();
+			//	}
 
 			socket.close();	
 			System.out.println("DONE");
@@ -121,10 +138,8 @@ public class Main {
 		//if file is complete set appropriate bool to true
 		f3Done = true;     
 	}
-
-
-
-
-
-
+	
+	public static void storeData(DatagramPacket packet, int fileID) {
+		//TODO: check file id and add data to respective array list
+	}
 }
